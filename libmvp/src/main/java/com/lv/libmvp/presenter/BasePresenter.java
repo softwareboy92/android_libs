3c4cd1e161dd0903ecd25aafbde4cf426759e17b
albert <www.lvzhongdi@icloud.com>
2019-10-11 13:37:50 +0800

    update basemvp presenter proxy

--- libmvp/src/main/java/com/lv/libmvp/presenter/BasePresenter.java
+++ libmvp/src/main/java/com/lv/libmvp/presenter/BasePresenter.java
@@ -7,6 +7,11 @@ import android.net.NetworkInfo;
 
 import com.lv.libmvp.contract.BaseContract;
 
+import java.lang.ref.WeakReference;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 import io.reactivex.disposables.CompositeDisposable;
 
 /**
@@ -18,25 +23,51 @@ public class BasePresenter<T extends BaseContract.BaseView> implements BaseContr
 
     protected T mView;
 
+    private WeakReference<T> mTWeakReference;
+
+
     protected CompositeDisposable disposables = new CompositeDisposable();
 
 
     @Override
     public void attachView(T view) {
-        this.mView = view;
+//        this.mView = view;
+        this.mTWeakReference = new WeakReference<T>(view);
+
+        //用代理对象，动态代理
+        mView = (T) Proxy.newProxyInstance(view.getClass().getClassLoader(), view.getClass().getInterfaces(), new InvocationHandler() {
+            @Override
+            public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                if (mTWeakReference == null && mTWeakReference.get()==null){
+                    return null;
+                }else {
+                    return method.invoke(mTWeakReference.get(),objects);
+                }
+
+            }
+        });
     }
 
     @Override
     public void detachView() {
-        if (mView != null) {
-            mView = null;
-        }
+//        if (mView != null) {
+//            mView = null;
+//        }
+        this.mTWeakReference.clear();
+        this.mTWeakReference = null;
+        this.mView = null;
+
+    }
+
+    public T getView() {
+        return mView;
     }
 
     @Override
     public void cancelAllRequest() {
         disposables.clear();
     }
+
 
     protected boolean checkNet(Context context) {
         return isNetworkConnected(context);
